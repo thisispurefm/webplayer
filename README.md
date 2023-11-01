cad102226369f4047f087bb50d542a705fbcb65e
HughTB <hugh.baldwin77@gmail.com>
2023-11-01 17:21:38 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# webplayer
+
+A pure JavaScript radio webplayer for PureFM.  
+  
+Should be *slightly* responsive for displays of different shapes, but primarily intended to be used in a popup window from the main site.  
+Includes such amazing features as:
+- A volume slider
+- LocalStorage for volume settings
+- A pause button?
---
+++ player.js
@@ -0,0 +1,38 @@
+// URL for the MP3 stream source
+const stream_url = "https://solid2.streamupsolutions.com/proxy/wofsojak?mp=/;type=mp3";
+
+// Prevent browser caching a few seconds when unpaused
+function getRandomStreamId(stream_url) {
+    return stream_url + '&' + Math.floor((Math.random() * 10000) + 1);
+}
+
+const player = document.getElementById('audio');
+const playbutton = document.getElementById('playbutton');
+const playbutton_icon = document.getElementById('play-pause');
+
+// Add event listener to play button
+playbutton.addEventListener('click', (evt) => {
+    if (player.paused) {
+        player.src = 'about:blank'; // Reset the audio element
+        player.pause();
+        player.src = getRandomStreamId(stream_url);
+        player.load();
+        player.play();
+        playbutton_icon.classList = "fa-solid fa-pause"
+    } else {
+        player.src = 'about:blank';
+        player.pause();
+        playbutton_icon.classList = "fa-solid fa-play"
+    }
+});
+
+function SetVolume(value) {
+    document.getElementById('audio').volume = value / 100;
+    localStorage.setItem("volume", value);
+}
+
+// Get the volume setting from localstorage, or 100% if not in there
+window.onload = function () {
+    document.getElementById('volume-control').value = localStorage.getItem("volume") ?? 100;
+    document.getElementById('audio').volume = (localStorage.getItem("volume") ?? 100) / 100;
+}
